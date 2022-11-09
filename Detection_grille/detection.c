00f113f098e962b0ace91ca77a61f2cc9a19dc85
Delphine Morvan <delphine.morvan@epita.fr>
2022-11-09 13:39:38 +0100

    update detection hough transform

--- Detection_grille/detection.c
+++ Detection_grille/detection.c
@@ -1,4 +1,5 @@
-#include "../Traitement/use.h"
+#include "use.h"
+#include <stdio.h>
 #include <math.h>
 
 
@@ -9,11 +10,11 @@ void foreach_pixels(Uint32 pixel_color, SDL_PixelFormat* format,
     SDL_GetRGB(pixel_color, format, &r, &g, &b);
     if (r != 0 && b != 0 && g != 0)
     {
-        for(float t = 0; t < 180; t++)
+        for(float t = 0; t < M_PI; t++)
         {
-            x = x * cos(t);
-            y = y * sin(t);
-            SDL_RenderDrawPoint(renderer,x,y);
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            int r = x * cos(t) + y * sin(t);
+            SDL_RenderDrawPoint(renderer,r,t);
         }
     }
 }
@@ -28,17 +29,44 @@ void hough_function(SDL_Surface* surface,SDL_Renderer* renderer)
 
     if(SDL_LockSurface(surface) != 0)
         errx(EXIT_FAILURE, "%s", SDL_GetError());
+    SDL_LockSurface(surface);
 
     // Try if a pixel is important or not and if it is, continue in another function
     for(int i = 0; i < w; i++)
     {
         for(int j = 0; j < h; j++)
         {
-            foreach_pixels(pixels[j*w + w],format,renderer,w,h);
+            foreach_pixels(pixels[j*i + j],format,renderer,j,i);
         }
     }
 
     SDL_UnlockSurface(surface);
+}
+
+void event_loop(SDL_Surface* surface,SDL_Renderer* renderer)
+{
+    // Creates a variable to get the events.
+    SDL_Event event;
+
+    while (1)
+    {
+        // Waits for an event.
+        SDL_WaitEvent(&event);
+
+        switch (event.type)
+        {
+            // If the "quit" button is pushed, ends the event loop.
+            case SDL_QUIT:
+                return;
+
+            case SDL_WINDOWEVENT:
+                if (event.window.event == SDL_WINDOWEVENT_RESIZED)
+                {
+                    hough_function(surface,renderer);
+                }
+                break;
+        }
+    }
 }
 
 int main(int argc, char** argv)
@@ -64,9 +92,6 @@ int main(int argc, char** argv)
     if (renderer == NULL)
         errx(EXIT_FAILURE, "%s", SDL_GetError());
 
-
-
-
     // Creates a window.
     SDL_Window* window1 = SDL_CreateWindow("Plain Window", 0, 0,100, 100,
             SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
@@ -79,19 +104,21 @@ int main(int argc, char** argv)
         errx(EXIT_FAILURE, "%s", SDL_GetError());
 
 
-
-
-
     // Create a surface and a texture from the image in argument
     SDL_Surface* s = load_image(argv[1]);
     SDL_Texture* t =  SDL_CreateTextureFromSurface(renderer, s);
+    printf("pour l'instant ca va");
 
-    //Go to the fonction for each pixels
+    //Initialise the window we are working on
+    SDL_SetRenderDrawColor(renderer1, 0, 0, 0, 255);
+    SDL_RenderClear(renderer1);
+
+
+    //Call the hough function
     hough_function(s,renderer1);
-
-    if(SDL_SaveBMP(s, "test_grayscale.bmp"))
-        printf("ERROR: %s", SDL_GetError());
-    SDL_FreeSurface(s);
+    SDL_RenderPresent(renderer1);
+    event_loop(s,renderer1);
+    printf("c'est passe");
 
     // Destroys the objects.
     SDL_DestroyRenderer(renderer);

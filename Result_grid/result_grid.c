58b88ee82b3c2a5a0e6293072e9d89b2d4691b67
Delphine Morvan <delphine.morvan@epita.fr>
2022-12-07 22:05:39 +0100

    Result_grid

--- Result_grid/result_grid.c
+++ Result_grid/result_grid.c
@@ -1,63 +1,91 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <err.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include "use.h"
 
-void draw(SDL_Renderer* renderer, int w, int h)
+// Updates the display.
+//
+// renderer: Renderer to draw on.
+// texture: Texture that contains the image.
+void draw(SDL_Renderer* renderer, SDL_Texture* texture)
 {
-    // Clears the renderer (sets the background to black).
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-
-    // Sets the color for drawing operations to white.
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-
-    // Draws the two diagonals.
-    SDL_RenderDrawLine(renderer, 0, 0, w, h);
-    SDL_RenderDrawLine(renderer, 0, h, w, 0);
-
-    // Updates the display.
+    SDL_RenderCopy(renderer,texture,NULL,NULL);
     SDL_RenderPresent(renderer);
+}
+
+Uint32 pixel_to_grayscale(Uint32 pixel_color, SDL_PixelFormat* format)
+{
+    Uint8 r, g, b;
+    SDL_GetRGB(pixel_color, format, &r, &g, &b);
+
+    Uint32 average = 0.3*r + 0.59*g + 0.11*b;
+    
+    r = average;
+    g = average;
+    b = average;
+
+    
+    Uint32 color = SDL_MapRGB(format, r, g, b);
+    return color; 
+}
+
+void fill_box(SDL_Surface* s, int w, int h, int len)
+{
+    s
+}
+
+void fill_grid(SDL_Surface* surface)
+{
+    Uint32* pixels = surface->pixels;
+    SDL_PixelFormat* format = surface->format;
+    
+    if (SDL_LockSurface(surface) != 0)
+        errx(EXIT_FAILURE, "%s", SDL_GetError());
+    
+    SDL_LockSurface(surface);
+    int h = (surface->h)/9;
+    int w = (surface->w)/9;
+    for (int j = 1; j < 9;j++)
+    {
+        fill_box(surface,w*j,h*j,w);
+    }
+    for(int j = 1; j < 9; j++)
+    {
+        int h = (surface->h)/9;
+        h *= j;
+        for(int x = 0; x < surface->w; x++)
+            pixels[h*(surface->w) + x] = SDL_MapRGB(surface->format,0,255,0);
+    }
+
+    for(int j = 1; j < 9; j++)
+    {
+        int w = (surface->w)/9;
+        w *= j;
+        for(int y = 0; y < surface->h; y++)
+            pixels[(surface->w)*y + w] = SDL_MapRGB(surface->format,255,0,0);
+    }
+
+    SDL_UnlockSurface(surface);
 }
 
 int main(int argc, char** argv)
 {
-    // Checks the number of arguments.
     if (argc != 2)
         errx(EXIT_FAILURE, "Usage: image-file");
-
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
-
-    // Creates a window.
-    SDL_Window* window = SDL_CreateWindow("RESULT", 0, 0, 100, 100,
+    SDL_Window* window = SDL_CreateWindow("Result", 0, 0, 100, 100,
             SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-
-    if (window == NULL)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
-
-    // Creates a renderer
-    SDL_Renderer* renderer =
-        SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (renderer == NULL)
         errx(EXIT_FAILURE, "%s", SDL_GetError());
-
-    SDL_Surface* s = load_image(argv[1]);
-    SDL_Texture* t =  SDL_CreateTextureFromSurface(renderer, s);
-
-    draw(renderer,s->w,s->h);
-
-    if(SDL_SaveBMP(s, "test_grayscale.bmp"))
-        printf("ERROR: %s", SDL_GetError());
-    SDL_FreeSurface(s);
-    // Destroys the objects.
+    SDL_Surface* surface = load_image(argv[1]);
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    fill_grid(surface);
+    SDL_SaveBMP(surface,"Result_grid.bmp");
+    SDL_FreeSurface(surface);
+    SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
-    SDL_DestroyTexture(t);
     SDL_DestroyWindow(window);
     SDL_Quit();
-
 
     return EXIT_SUCCESS;
 }

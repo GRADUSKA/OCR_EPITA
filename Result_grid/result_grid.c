7f26668fd9b50c169aeee827130d053040343bc7
Delphine Morvan <delphine.morvan@epita.fr>
2022-12-08 01:07:50 +0100

    The final grid is okay

--- Result_grid/result_grid.c
+++ Result_grid/result_grid.c
@@ -3,41 +3,66 @@
 #include <SDL2/SDL_image.h>
 #include "use.h"
 
-// Updates the display.
-//
-// renderer: Renderer to draw on.
-// texture: Texture that contains the image.
-void draw(SDL_Renderer* renderer, SDL_Texture* texture)
-{
-    SDL_RenderCopy(renderer,texture,NULL,NULL);
-    SDL_RenderPresent(renderer);
-}
-
-Uint32 pixel_to_grayscale(Uint32 pixel_color, SDL_PixelFormat* format)
+int test(Uint32 pixel_color, SDL_PixelFormat* format)
 {
     Uint8 r, g, b;
     SDL_GetRGB(pixel_color, format, &r, &g, &b);
 
-    Uint32 average = 0.3*r + 0.59*g + 0.11*b;
-    
-    r = average;
-    g = average;
-    b = average;
-
-    
-    Uint32 color = SDL_MapRGB(format, r, g, b);
-    return color; 
+    if(r < 125 && g < 125 && b < 125)
+        return 0;
+    return 1;
 }
 
-void fill_box(SDL_Surface* s, int w, int h, int len)
+void fill_box(SDL_Surface* s, int w, int h, int len,Uint32* pix,int color,int number)
 {
-    s
+
+    SDL_Surface* surface = load_image("Source/8.jpg");
+
+    if(number == 1)
+    {
+        surface = load_image("Source/1.jpg");
+    }
+    else if(number == 2)
+    {
+        surface = load_image("Source/2.jpg");
+    }
+    else if(number == 3)
+    {
+        surface = load_image("Source/3.jpg");
+    }
+    else if(number == 4)
+    {
+        surface = load_image("Source/4.jpg");
+    }
+    else if(number == 5)
+    {
+        surface = load_image("Source/5.jpg");
+    }
+    else if(number == 6)
+    {
+        surface = load_image("Source/6.jpg");
+    }
+    else if(number == 7)
+    {
+        surface = load_image("Source/7.jpg");
+    }
+
+    Uint32* pixels_number = surface->pixels;
+    SDL_PixelFormat* format = surface->format;
+    for(int i = 0; i < surface->h;i++)
+    {
+        for(int j = 0; j < surface->w; j++)
+        {
+            if (test(pixels_number[i*(surface->w) + j],format) == 0)
+                pix[(h+i)*(s->w) + (w+j)] = SDL_MapRGB(surface->format,color,0,0); 
+        }
+    }
+    SDL_FreeSurface(surface);
 }
 
-void fill_grid(SDL_Surface* surface)
+void fill_grid(SDL_Surface* surface,int* tab,int* origin)
 {
     Uint32* pixels = surface->pixels;
-    SDL_PixelFormat* format = surface->format;
     
     if (SDL_LockSurface(surface) != 0)
         errx(EXIT_FAILURE, "%s", SDL_GetError());
@@ -45,26 +70,18 @@ void fill_grid(SDL_Surface* surface)
     SDL_LockSurface(surface);
     int h = (surface->h)/9;
     int w = (surface->w)/9;
-    for (int j = 1; j < 9;j++)
+    int color = 0;
+    for (int j = 0; j < 9;j++)
     {
-        fill_box(surface,w*j,h*j,w);
+        for(int u = 0; u < 9; u++)
+        {
+            if(tab[j*9+u] != origin[j*9+u])
+                color = 255;
+            else
+                color=0;
+            fill_box(surface,w*u,h*j,w,pixels,color,tab[j*9+u]);
+        }
     }
-    for(int j = 1; j < 9; j++)
-    {
-        int h = (surface->h)/9;
-        h *= j;
-        for(int x = 0; x < surface->w; x++)
-            pixels[h*(surface->w) + x] = SDL_MapRGB(surface->format,0,255,0);
-    }
-
-    for(int j = 1; j < 9; j++)
-    {
-        int w = (surface->w)/9;
-        w *= j;
-        for(int y = 0; y < surface->h; y++)
-            pixels[(surface->w)*y + w] = SDL_MapRGB(surface->format,255,0,0);
-    }
-
     SDL_UnlockSurface(surface);
 }
 
@@ -72,19 +89,10 @@ int main(int argc, char** argv)
 {
     if (argc != 2)
         errx(EXIT_FAILURE, "Usage: image-file");
-    SDL_Window* window = SDL_CreateWindow("Result", 0, 0, 100, 100,
-            SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (renderer == NULL)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
     SDL_Surface* surface = load_image(argv[1]);
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-    fill_grid(surface);
+    //fill_grid(surface,tab,tab2);
     SDL_SaveBMP(surface,"Result_grid.bmp");
     SDL_FreeSurface(surface);
-    SDL_DestroyTexture(texture);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
     SDL_Quit();
 
     return EXIT_SUCCESS;
